665efe86b2a7e2d91084fe7991d8bbd86219ad10
valeriamorra <valeria.morra@LEGO.com>
2024-04-05 14:06:22 +0200

    Vales changes
    I made my first change on the C# code yay

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("The current time is " + DateTime.Now);
+
+Console.WriteLine("Hi this is Vales funny branch");
