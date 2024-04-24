a6dd7865e20721ef8328102376f91774fd45445a
valeriamorra <valeria.morra@LEGO.com>
2024-04-24 09:33:19 +0200

    Function in C#
    Function that make 2 transformation to the Hello World string

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -3,3 +3,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("The current time is " + DateTime.Now);
 
 Console.WriteLine("Hi this is Vales funny branch");
+
+string str = "Hello, world!";
+        Console.WriteLine(str.ToUpper());
+        Console.WriteLine(str.Replace('o', 'x'));
